d0e9739edb29ed25b1a8457d88856b4dcbc9a862
s3anfinnegan <sean2001finnegan@gmail.com>
2025-10-05 15:07:35 +0100

    Changing PORT to ensure Render server deployment

--- api/server.js
+++ api/server.js
@@ -45,5 +45,6 @@ ${message}
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; // use 5000 locally if PORT is undefined
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+

ce59c346b591314d20caee31ae008321877d896f
s3anfinnegan <sean2001finnegan@gmail.com>
2025-10-05 12:59:29 +0100

    Moving files

---
+++ client/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/finn-elec/", // 👈 very important for GitHub Pages
+});

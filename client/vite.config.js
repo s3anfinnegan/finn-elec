677dd98e47307017b10342238d090976b00a8a03
s3anfinnegan <sean2001finnegan@gmail.com>
2025-10-05 13:38:42 +0100

    Updated Vite config for correct rendering on webapp

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/finn-elec/", // 👈 very important for GitHub Pages
+  //base: "/finn-elec/", // 👈 very important for GitHub Pages
+  base: "/",
 });
